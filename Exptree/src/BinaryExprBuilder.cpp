ff3c795580856518120dc0ba24c72a40ddc58288
Aaront <aarontr2409@gmail.com>
2016-05-12 21:37:56 -0500

    Avance de Parser 2
    Se implemento la function parser, aun falta probarlo.

--- Exptree/src/BinaryExprBuilder.cpp
+++ Exptree/src/BinaryExprBuilder.cpp
@@ -1,18 +1,78 @@
 #include "BinaryExprBuilder.h"
+#include "NumElemNode.h"
+#include <sstream>
+#include <iostream>
+
+using namespace std;
 
 BinaryExprBuilder::BinaryExprBuilder()
 {
     //ctor
 }
 
-BinaryOpNode *BinaryExprBuilder::parse(string &istr)
+BinaryOpNode *BinaryExprBuilder::parse(string &str)
 {
+    istringstream istr(str); //convierte el string en un vector
+    char token;
 
+    while (istr >> token)
+    {
+
+        switch (token)
+        {
+
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                processOperator(token);
+                break;
+
+            case ')':
+                processRightParenthesis();
+                break;
+
+            case '(':
+                operatorStack.push(token);
+                break;
+
+            default:
+                istr.putback(token);
+
+                double number;
+                istr >> number;
+                //cout<<number<<endl;
+
+                NumElemNode *newNode = new NumElemNode(number);
+                operandStack.push(newNode);
+
+                continue;
+        }
+    }
+
+    while (!operatorStack.empty())
+    {
+
+        doBinary(operatorStack.top());
+        operatorStack.pop();
+    }
+
+
+    ExprElemNode *p = operandStack.top();
+
+    return static_cast<BinaryOpNode *> (p);
 }
 
 void BinaryExprBuilder::processOperator(char op)
 {
+    int opPrecedence = precedence(op);
 
+    while((!operatorStack.empty()) && (opPrecedence <= precedence(operatorStack.top())))
+    {
+        doBinary(operatorStack.top());
+        operatorStack.pop();
+    }
+    operatorStack.push(op);
 }
 
 void BinaryExprBuilder::processRightParenthesis()
